cc37c95ba71e95142dec38cb89c24ca6ce867e49
Isabela <isabelacristina20005@gmail.com>
2024-02-24 15:38:25 -0300

    To do list

---
+++ tarefas/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework import routers
+from .viewsets import TarefasViewSet
+
+app_name = 'tarefas'
+router = routers.DefaultRouter()
+router.register(r'tarefas', TarefasViewSet, basename='Tarefas')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
---
+++ tarefas/api/viewsets.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets, status
+from tarefas.models import Tarefa
+from .serializers import TarefaSerializer, TarefaDetalhadaSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+class TarefasViewSet(viewsets.ModelViewSet):
+    serializer_class = TarefaSerializer
+    queryset = Tarefa.objects.order_by('data_criacao')
+    
+    def get_serializer_class(self):
+        if self.action== 'retrieve':
+            return TarefaDetalhadaSerializer
+        return TarefaSerializer
+    
+    @action(detail=True, methods=['get'])
+    def concluida(self, request, pk=None):
+        try:
+            tarefa = self.get_object()
+        except Tarefa.DoesNotExist:
+            return Response({"error": "Tarefa não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+        tarefa.concluida = True
+        tarefa.save()
+        serializer = self.get_serializer(tarefa)
+        return Response(serializer.data)
